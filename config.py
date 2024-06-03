8a55b747a696b2fc95112929ddcba916d3591503
Dhiman <103347834+ds98762@users.noreply.github.com>
2024-06-03 23:01:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/46e9050e979b083556fdd.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/46e9050e979b083556fdd.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Chxting")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Ct_x07")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
